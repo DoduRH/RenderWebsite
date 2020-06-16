e6b74ae2882ca30145ccd6557c9e238501894f31
Josh <yr18478@bristol.ac.uk>
2020-06-16 20:56:17 +0100

    Getting FAQ page ready

--- Website/main.py
+++ Website/main.py
@@ -373,6 +373,11 @@ def download():
     return redirect(url)
 
 
+@app.route('/faq')
+def faq():
+    return app.send_static_file('html/faq.html')
+
+
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
     # Engine, a webserver process such as Gunicorn will serve the app. This
